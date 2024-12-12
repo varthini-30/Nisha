a7fb092532497b376c16a9f29c19ae0be9aaafaf
Nishanthi1912 <24ucb135nisanthi@kgkite.ac.in>
2024-12-12 12:35:27 +0530

    Nishanthi error corrected

--- easyproblem.py
+++ easyproblem.py
@@ -4,11 +4,11 @@ def last_char_of_string(s):
     else:
         return ""  # Return an empty string for empty input
 
-if _name__ == "__main_":
-    a_string = int(input("Enter a string: "))
-    last_char = last_char_of_string(a string)
+if __name__ == "__main__":
+    a_string = input("Enter a string: ")
+    last_char = last_char_of_string(a_string)
     if last_char:
       
-      print("The last character of the string is:"  last char)
-    else;
+      print("The last character of the string is:" ,last_char)
+    else:
       print("The string is empty.")  
