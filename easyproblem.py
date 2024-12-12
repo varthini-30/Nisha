b20448a0cbd5c615131a32385ed98e1d9881d4fa
Sudarshan-prog <epicvisionsan@gmail.com>
2024-12-12 11:17:20 +0530

    Add files via upload

---
+++ easyproblem.py
@@ -0,0 +1,14 @@
+def last_char_of_string(s):
+    if s:
+        return s[-1]
+    else:
+        return ""  # Return an empty string for empty input
+
+if _name__ == "__main_":
+    a_string = int(input("Enter a string: "))
+    last_char = last_char_of_string(a string)
+    if last_char:
+      
+      print("The last character of the string is:"  last char)
+    else;
+      print("The string is empty.")  
